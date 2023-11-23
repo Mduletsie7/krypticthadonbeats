04f5538afe28df7ca4aeddfcf8e4cb400390012e
Mdumisi Kelvin Letsie <mdu.letsie7@gmail.com>
2023-11-23 23:56:00 +0200

    Temporarily hide discography section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Header from './components/Header';
 import Hero from './components/Hero';
-import Discography from './components/Discography';
+// import Discography from './components/Discography';
 import Footer from './components/Footer';
 
 
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <Header />
       <Hero />
-      <Discography />
+      {/* <Discography /> */}
       <Footer />
     </div>
   );
