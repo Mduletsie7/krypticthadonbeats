7f11e840062d7be7762bdb0e514f9c10ad208a5c
Mdumisi Kelvin Letsie <mdu.letsie7@gmail.com>
2023-11-26 14:39:57 +0200

    Added beat license pricing table information

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import "primeflex/primeflex.css";
 import Header from './components/Header';
 import Hero from './components/Hero';
 import LeaseBeats from './components/LeaseBeats';
+import { PricingTable } from './components/PricingTable';
 import NotableCredits from './components/NotableCredits';
 import Footer from './components/Footer';
-
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <Hero />
       <LeaseBeats />
+      <PricingTable />
       <h1>Production Credits</h1>
       <NotableCredits />
       <Footer />
