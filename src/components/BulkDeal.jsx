f6568e79c5e2ff045afc25a67a9eddfbd064e0bd
Mdumisi Kelvin Letsie <mdu.letsie7@gmail.com>
2024-03-29 09:46:56 +0200

    Added deals section

---
+++ src/components/BulkDeal.jsx
@@ -0,0 +1,21 @@
+// HeroSection.js
+import React from "react";
+import "../styles/Bulk.css";
+// import image from "../assets/kryptic.png";
+
+const BulkDeal = () => {
+  return (
+    <section className="bulk">
+      <div className="bulk-content">
+        <div className="bulk-text">
+          <h1 className="bulk-deal-header">BUY 1 GET 1 FREE</h1>
+          <p>
+            Simply Add 2 Beats To Cart To Get 1 Free 🛒
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default BulkDeal;
